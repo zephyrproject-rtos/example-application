d1c935e55c49a905af2c4e98c4567c3dd1273eb5
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2021-04-18 20:47:28 +0200

    app: initial application skeleton
    initial application skeleton demonstrating:
    
    - custom boards
    - custom DT bindings
    - Out-of-tree drivers
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

---
+++ app/src/foo.h
@@ -0,0 +1,30 @@
+/**
+ * @file foo.h
+ *
+ * Foo API.
+ *
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _FOO_H_
+#define _FOO_H_
+
+/**
+ * @defgroup app_foo Foo API
+ *
+ * A detailed description of the foo API.
+ *
+ * @{
+ */
+
+/**
+ * @brief An example foo API call.
+ *
+ * @return 0 on success, negative errno otherwise.
+ */
+int app_foo_bar(void);
+
+/** @} */
+
+#endif /* _FOO_H_ */
