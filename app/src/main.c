765d7299d5ef23246b0a2d05a836efeb6847986c
Gerard Marull-Paretas <gerard@teslabs.com>
2022-05-06 11:33:11 +0200

    app, drivers: migrate includes to <zephyr/...>
    Zephyr includes are now prefixed with <zephyr/...>. While the old path
    can still be used when CONFIG_LEGACY_INCLUDE_PATH=y, it's better to be
    prepared for the future.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- app/src/main.c
+++ app/src/main.c
@@ -3,12 +3,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <kernel.h>
-#include <drivers/sensor.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/sensor.h>
 
 #include "app_version.h"
 
-#include <logging/log.h>
+#include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 void main(void)
