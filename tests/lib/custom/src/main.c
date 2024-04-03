3ca6216b52e4dfacd561f345cb5e5e0574e3b1ee
Gerard Marull-Paretas <gerard@teslabs.com>
2024-04-03 13:45:33 +0200

    lib: custom: simplify naming scheme
    Improve the naming scheme of the custom library.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- tests/lib/custom/src/main.c
+++ tests/lib/custom/src/main.c
@@ -15,27 +15,27 @@
 
 #include <zephyr/ztest.h>
 
-#include <app/custom_lib/custom_lib.h>
+#include <app/lib/custom.h>
 
 ZTEST(custom_lib, test_get_value)
 {
 	/* Verify standard behavior */
-	zassert_equal(custom_lib_get_value(INT_MIN), INT_MIN,
+	zassert_equal(custom_get_value(INT_MIN), INT_MIN,
 		"get_value failed input of INT_MIN");
-	zassert_equal(custom_lib_get_value(INT_MIN + 1), INT_MIN + 1,
+	zassert_equal(custom_get_value(INT_MIN + 1), INT_MIN + 1,
 		"get_value failed input of INT_MIN + 1");
-	zassert_equal(custom_lib_get_value(-1), -1,
+	zassert_equal(custom_get_value(-1), -1,
 		"get_value failed input of -1");
-	zassert_equal(custom_lib_get_value(1), 1,
+	zassert_equal(custom_get_value(1), 1,
 		"get_value failed input of 1");
-	zassert_equal(custom_lib_get_value(INT_MAX - 1), INT_MAX - 1,
+	zassert_equal(custom_get_value(INT_MAX - 1), INT_MAX - 1,
 		"get_value failed input of INT_MAX - 1");
-	zassert_equal(custom_lib_get_value(INT_MAX), INT_MAX,
+	zassert_equal(custom_get_value(INT_MAX), INT_MAX,
 		"get_value failed input of INT_MAX");
 
 	/* Verify override behavior */
-	zassert_equal(custom_lib_get_value(0),
-		CONFIG_CUSTOM_LIB_GET_VALUE_DEFAULT,
+	zassert_equal(custom_get_value(0),
+		CONFIG_CUSTOM_GET_VALUE_DEFAULT,
 		"get_value failed input of 0");
 }
 
