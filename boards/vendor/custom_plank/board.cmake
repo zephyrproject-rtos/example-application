ea9e6d1ae20a1bb08b88941f466201e033003548
Pieter De Gendt <pieter.degendt@basalte.be>
2024-12-20 10:26:08 +0100

    boards: custom_plank: Add demo out-of-tree runner
    Demonstrate how to use an out-of-tree runner with the custom_plank
    board.
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- boards/vendor/custom_plank/board.cmake
+++ boards/vendor/custom_plank/board.cmake
@@ -3,6 +3,7 @@
 
 board_runner_args(jlink "--device=nrf52" "--speed=4000")
 board_runner_args(pyocd "--target=nrf52840" "--frequency=4000000")
+board_runner_args(example "--param=foo")
 
 set(OPENOCD_NRF5_SUBFAMILY "nrf52")
 
@@ -10,3 +11,4 @@ include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../../common/example_runner.board.cmake)
