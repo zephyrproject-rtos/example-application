10eb1bbd7e88fc328aed08a7af9247c940b0b910
Osama <osama.salahuddin@gmail.com>
2025-03-19 16:47:04 +0100

    - Wifi Build for Esp32

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -6,16 +6,26 @@
 
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/net/net_event.h>
 
-int main()
+int main(void)
 {
-    /* Main Function */
-    std::cout<<"Hello World!\n";
+    // struct net_if *iface = net_if_get_default();
 
-    while(1)
-    {
-        /* Infinite Loop */
-        k_msleep(1000);
-    }
+    /* Main Function */
+    printk("Hello World!\n");
+
+    // if (iface)
+    // {
+    //     net_if_up(iface);
+    //     printk("Network interface found!\n");
+    // }
+    // else
+    // {
+    //     printk("No network interface found!\n");
+    // }
+
     return 0;
 }
