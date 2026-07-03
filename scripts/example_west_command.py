03d5bd60bf11814f603e113622b14051f3bf3bae
Daniel DeGrasse <daniel.degrasse@analog.com>
2026-07-03 19:55:21 +0000

    scripts: format example_west_command to follow ruff guidelines
    Format example_west_command to comply with ruff CI checks
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@analog.com>

--- scripts/example_west_command.py
+++ scripts/example_west_command.py
@@ -8,11 +8,11 @@ Example of a west extension in the example-application repository.'''
 
 from west.commands import WestCommand  # your extension must subclass this
 
-class ExampleWestCommand(WestCommand):
 
+class ExampleWestCommand(WestCommand):
     def __init__(self):
         super().__init__(
-            'example-west-command',               # gets stored as self.name
+            'example-west-command',  # gets stored as self.name
             'an example west extension command',  # self.help
             # self.description:
             '''\
@@ -22,21 +22,20 @@ You can split this up into multiple paragraphs and they'll get
 reflowed for you. You can also pass
 formatter_class=argparse.RawDescriptionHelpFormatter when calling
 parser_adder.add_parser() below if you want to keep your line
-endings.''')
+endings.''',
+        )
 
     def do_add_parser(self, parser_adder):
         # This is a bit of boilerplate, which allows you full control over the
         # type of argparse handling you want. The "parser_adder" argument is
         # the return value of an argparse.ArgumentParser.add_subparsers() call.
-        parser = parser_adder.add_parser(self.name,
-                                         help=self.help,
-                                         description=self.description)
+        parser = parser_adder.add_parser(self.name, help=self.help, description=self.description)
 
         # Add some example options using the standard argparse module API.
         parser.add_argument('-o', '--optional', help='an optional argument')
         parser.add_argument('required', help='a required argument')
 
-        return parser           # gets stored as self.parser
+        return parser  # gets stored as self.parser
 
     def do_run(self, args, unknown_args):
         # This gets called when the user runs the command, e.g.:
